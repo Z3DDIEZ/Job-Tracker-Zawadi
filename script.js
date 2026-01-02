3bf845c17f1f7dfa67028691258e3cf4f8d14958
Z3DDIEZ <zawadinyachiya4@gmail.com>
2026-01-02 23:55:03 +0200

    Added boilerplate header and footer to HTML file

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Test Firebase connection
-console.log(
-  "Firebase initialized:",
-  firebase.apps.length > 0
-);
-
-// Test writing data
-function testFirebase() {
-  database
-    .ref("test")
-    .set({
-      message: "Hello Firebase!",
-      timestamp: Date.now()
-    })
-    .then(() => {
-      console.log("Data written to Firebase");
-    })
-    .catch((error) => {
-      console.error("Error writing to Firebase:", error);
-    });
-}
-
-// Call this function to test
-testFirebase();
+// Your app code will go here
+console.log("Job Tracker initialized");
+console.log("Firebase connected:", firebase.apps.length > 0);
